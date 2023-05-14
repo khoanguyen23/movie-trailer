563c411b1893c254b90656154ef826dcff6b631e
khoanguyen23 <khoahoang151@gmail.com>
2023-05-14 22:02:58 +0700

    add Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow : {
+        '3xl': '0 0 7px 15px rgba(255,0,0,.302)',
+      }
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
