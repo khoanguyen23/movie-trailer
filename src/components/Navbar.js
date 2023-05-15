a57dfa711535773c9741853ef41b336f638c065a
khoanguyen23 <khoahoang151@gmail.com>
2023-05-15 09:13:51 +0700

    add Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
-        MOVIE TRAILER
+        MOVLER
       </h1>
       <div className="px-3">
-        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+        <button className="text-white pr-4">
           Sign in
         </button>
         <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
