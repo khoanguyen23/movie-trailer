7021bdad03248f0c12bc6c5849c39b340b545d36
khoanguyen23 <khoahoang151@gmail.com>
2023-05-24 09:43:27 +0700

    [add] row component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row title = 'Up Coming' fetchURL= {requests.requestUpcoming} />
+      <Row title = 'Popular' fetchURL= {requests.requestUpcoming} />
+      <Row title = 'Top Rated' fetchURL= {requests.requestUpcoming} />
+      <Row title = 'Trending' fetchURL= {requests.requestUpcoming} />
+      <Row title = 'Horror' fetchURL= {requests.requestUpcoming} />
     </>
   )
 }
