4adad4f5d989023a70c72808d4283485a9d7fa28
khoanguyen23 <khoahoang151@gmail.com>
2023-05-29 10:56:02 +0700

    [add] scroll function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa';
 const Movie = ({ item, id }) => {
   const [like, setLike] = useState(false);
   return (
-    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 ml-[3px]">
+    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
         className="w-full h-auto block"
         src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Main from '../components/Main'
-import Row from '../components/Row'
-import requests from '../Requests'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 const Home = () => {
   return (
     <>
       <Main />
-      <Row title = 'Up Coming' fetchURL= {requests.requestUpcoming} />
-      <Row title = 'Popular' fetchURL= {requests.requestUpcoming} />
-      <Row title = 'Top Rated' fetchURL= {requests.requestUpcoming} />
-      <Row title = 'Trending' fetchURL= {requests.requestUpcoming} />
-      <Row title = 'Horror' fetchURL= {requests.requestUpcoming} />
+      <Row rowID="1" title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
