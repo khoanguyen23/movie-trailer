8ba606197e81c8c2798301e889eaf45eb19b0f88
khoanguyen23 <khoahoang151@gmail.com>
2023-05-30 23:19:12 +0700

    [add] authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  // console.log(user.email)
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <Link to='/'>
@@ -9,8 +23,21 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
-      <div className="px-3">
-      <Link to='/login'>
+      {user?.email ? (
+        <div>
+          <Link to='/account'>
+            <button className='text-white pr-4'>Account</button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Link to='/login'>
             <button className='text-white pr-4'>Sign In</button>
           </Link>
           <Link to='/signup'>
@@ -18,7 +45,8 @@ const Navbar = () => {
               Sign Up
             </button>
           </Link>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className='w-full h-screen'>
@@ -16,18 +32,18 @@ const Signup = () => {
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
               <form
-                
+                onSubmit={handleSubmit}
                 className='w-full flex flex-col py-4'
               >
                 <input
-                 
+                  onChange={(e) => setEmail(e.target.value)}
                   className='p-3 my-2 bg-gray-700 rouded'
                   type='email'
                   placeholder='Email'
                   autoComplete='email'
                 />
                 <input
-                  
+                  onChange={(e) => setPassword(e.target.value)}
                   className='p-3 my-2 bg-gray-700 rouded'
                   type='password'
                   placeholder='Password'
@@ -55,7 +71,7 @@ const Signup = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
